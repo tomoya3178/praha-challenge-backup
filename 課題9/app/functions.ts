c7c6b5a9f6e8e9d746664b917ac2bb21bce23207
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2021-10-31 23:06:02 +0900

    回答

---
+++ 課題9/app/functions.ts
@@ -0,0 +1,43 @@
+import { NameApiService, NameApiServiceInterface } from "./nameApiService";
+import { DatabaseMock } from "./util";
+
+export const sumOfArray = (numbers: number[]): number => {
+  if (numbers.length === 0) {
+    return 0
+  }
+  return numbers.reduce((a: number, b: number): number => a + b);
+};
+
+export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
+  return new Promise((resolve): void => {
+    resolve(sumOfArray(numbers));
+  });
+};
+
+export const asyncSumOfArraySometimesZero = (
+  numbers: number[],
+  database: DatabaseMock
+): Promise<number> => {
+  return new Promise((resolve): void => {
+    try {
+      // const database = new DatabaseMock(); // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
+      database.save(numbers);
+      resolve(sumOfArray(numbers));
+    } catch (error) {
+      resolve(0);
+    }
+  });
+};
+
+export const getFirstNameThrowIfLong = async (
+  maxNameLength: number,
+  nameApiService: NameApiServiceInterface
+): Promise<string> => {
+  // const nameApiService = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
+  const firstName = await nameApiService.getFirstName();
+
+  if (firstName.length > maxNameLength) {
+    throw new Error("first_name too long");
+  }
+  return firstName;
+};
